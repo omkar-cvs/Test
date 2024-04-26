32a17337faab313d47dc3e34360615561d789a6c
Omkar Singh <omkarsinghaws@gmail.com>
2024-04-26 22:40:11 +0530

    Adding SpringBootServletInitializer

--- DemoApp/src/main/java/com/oks/demo/DemoAppApplication.java
+++ DemoApp/src/main/java/com/oks/demo/DemoAppApplication.java
@@ -2,9 +2,10 @@ package com.oks.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoAppApplication {
+public class DemoAppApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAppApplication.class, args);
